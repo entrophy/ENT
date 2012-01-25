5c9f16ee4359cf78a058de24522f14088ad19c40
Mick Hansen <doomschild@gmail.com>
2012-01-25 18:31:52 +0100

    Autoload update

--- src/functions.php
+++ src/functions.php
@@ -122,110 +122,16 @@ function createFolder($path) {
 
 function ENT_Autoload($class) {
 	if (strpos($class, 'ENT_') === 0) {
-		if (class_exists($class, false) || interface_exists($class, false)) {
-			return;
-		}
-		
 		require ENT::registry('ent_path').'src/'.str_replace(array('ENT_', '_'), array('', '/'), $class).'.php';
 	} else if (preg_match('/Controller$/', $class)) {
 		require ENT::registry('project_path').'app/code/controllers/'.str_replace("_", "/", $class).'.php';
 	} else if (strpos($class, 'Helper_') === 0) {
-	
+		require ENT::registry('project_path').'app/code/helpers/'.implode("/", array_slice(explode("_", $class), 1)).'.php';
+	} else if (preg_match('/_View$/', $class)) {
+		require ENT::registry('project_path').'app/code/views/'.implode("/", array_slice(explode("_", $class), 0, -1)).'.php';
+	} else {
+		require ENT::registry('project_path').'app/code/modules/'.str_replace("_", "/", $class).'.php';;
 	}
 }
 spl_autoload_register('ENT_Autoload');
-function __autoload($class) {
-	if (class_exists($class, false) || interface_exists($class, false)) {
-		return;
-	}
-
-	if (preg_match('/^ENT_/', $class)) {
-		$includePath = ENT::registry('ent_path').'src/';
-		$class = explode("_", $class);
-		$class[0] = null;
-		
-		$x = 1;
-		foreach ($class as $classItem) {
-			if ($classItem) {
-				$fileName .= $classItem;
-				if ($x != sizeof($class)) {
-					$fileName .= '/';
-				}
-			}	
-			$x++;
-		}
-		
-		$fileName = $fileName.".php";
-		$includePath .= $fileName;
-	
-		if (file_exists($includePath)) {
-			
-			require $includePath;
-		}
-	} else if (preg_match('/Controller$/', $class)) {
-		$class = str_replace("_", "/", $class);
-		
-		$controller_path = ENT::registry('project_path').'app/code/controllers/'.$class.'.php';
-		require $controller_path;
-	} else if (preg_match('/View$/', $class)) {
-		$class = explode("_", $class);
-	
-		$target = sizeof($class) - 1;
-		array_splice($class, $target, 1);
-		$class = implode("/", $class);
-
-		$view_path = ENT::registry('project_path').'app/code/views/'.$class.'.php';
-		require $view_path;
-	} else if (preg_match('/^Helper/', $class)) {
-		$class = explode("_", $class);
-		array_splice($class, 0, 1);
-
-		$class = implode("_", $class);
-	
-		$class = str_replace("_", "/", $class);
-		require ENT::registry('project_path').'app/code/helpers/'.$class.'.php';
-	} else {
-		$class = explode("_", $class);
-		
-		
-		switch ($class[0]) {
-			case 'ENT':
-				$class[0] = null;
-				$includePath = ENT::registry('ent_path');
-				break;
-			case 'jQueryTmpl':
-				$includePath = ENT::registry('lib_path').'/jquery-tmpl-php/';
-				$debug = true;
-				break;
-			case 'Zend':
-				set_include_path(ENT::registry('zend_path'));
-				$includePath = ENT::registry('zend_path') ? : '';
-				break;
-			default:
-				set_include_path('');
-				$includePath = ENT::registry('project_path').'app/code/modules/';
-				
-		}
-		
-		$x = 1;
-		foreach ($class as $classItem) {
-			if ($classItem) {
-				$fileName .= $classItem;
-				if ($x != sizeof($class)) {
-					$fileName .= '/';
-				}
-			}	
-			$x++;
-		}
-		
-		$fileName = $fileName.".php";
-		$includePath .= $fileName;
-		
-		if (file_exists($includePath)) {	
-			require $includePath;
-		}
-		
-		set_include_path('');
-	}
-}
 ?>

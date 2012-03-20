a96f7d7aca2a20dd28bd883a2b3486d8a9207d85
Mick Hansen <doomschild@gmail.com>
2012-03-20 17:02:07 +0100

    added ifPlural/ifSingular checks to Inflector

--- src/Inflector.php
+++ src/Inflector.php
@@ -1,18 +1,29 @@
 <?php
 class ENT_Inflector {
-	public static function pluralize($string) {
-		if (substr($string, -1, 1) == 'y') {
-			$string = substr_replace($string, 'ies', -1, 1);
-		} else {
-			$string .= 's';
+	public static function isPlural($string) {
+		return substr($string, -1, 1) == 's';
+	}
+	public static function pluralize($string, $check = false) {
+		if (!self::isPlural($string)) {
+			if (substr($string, -1, 1) == 'y') {
+				$string = substr_replace($string, 'ies', -1, 1);
+			} else {
+				$string .= 's';
+			}
 		}
 		return $string;
 	}
-	public static function singularize($string) {
-		if (substr($string, -3, 3) == 'ies') {
-			$string = substr_replace($string, 'y', -3, 3);
-		} elseif (substr($string, -1, 1) == 's') {
-			$string = substr_replace($string, '', -1, 1);
+	
+	public static function isSingular($string) {
+		return substr($string, -1, 1) != 's';
+	}
+	public static function singularize($string, $check = false) {
+		if (!self::isSingular($string)) {
+			if (substr($string, -3, 3) == 'ies') {
+				$string = substr_replace($string, 'y', -3, 3);
+			} elseif (substr($string, -1, 1) == 's') {
+				$string = substr_replace($string, '', -1, 1);
+			}
 		}
 		return $string;
 	}
